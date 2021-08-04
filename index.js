a90df246f3e1458b0858283de2a7705993088711
adelina2222 <adelinachiriac@gmail.com>
2021-08-04 11:24:03 +0100

    Implement recursive maze generation function

--- index.js
+++ index.js
@@ -58,4 +58,71 @@ const horizontals = Array(cells - 1)
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
-console.log(startRow, startColumn);
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    
+    return arr;
+};
+
+const moveThroughCell = (row, column) => {
+    // Check if cell at grid[row, column] has been visited already. If yes, return
+    if (grid[row][column]) {
+        return;
+    }
+
+    // Mark cell as being visited
+    grid[row][column] = true;
+    
+    // Create randomly-ordered list of neighbour cells
+    const neighbours = shuffle([
+        // Calculate coordinates of neighbour cells (based on current cell) and randomize the array by using the shuffle function
+        [row - 1, column, "up"],
+        [row, column + 1, "right"],
+        [row + 1, column, "down"],
+        [row, column - 1, "left"]
+    ]);
+
+    // Iterate through each neighbour
+    for (let neighbour of neighbours) {
+        // Extract the coordinates & direction of the neighbour we're thinking of visiting next
+        const [nextRow, nextColumn, direction] = neighbour;
+
+        // Check if this neighbour is out of bounds, and if yes, skip this element
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue;
+        }
+
+        // Check to see if we visited this neighbour already
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+
+        // Remove the wall from either the horizontals or verticals array, depending on the chosen direction
+        if (direction === "left") {
+            verticals[row][column - 1] = true;
+        }
+        else if (direction === "right") {
+            verticals[row][column] = true;
+        }
+        else if (direction === "up") {
+            horizontals[row - 1][column] = true;
+        }
+        else if (direction === "down") {
+            horizontals[row][column] = true;
+        }
+
+        // Move to the next cell
+        moveThroughCell(nextRow, nextColumn);
+    }
+};
+
+moveThroughCell(startRow, startColumn);
